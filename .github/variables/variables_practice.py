9ab5d67aff8ee470017d00c11112393a4d9d97e7
Tristan mckay <90346589+tmacbr@users.noreply.github.com>
2021-10-29 14:39:44 -0400

    version0.1
    added character lines and 3 type of data interger, floating point, and string

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# variablespractice, tristan mckay, 10/29/21 2:22pm, version 0.0 
+# variablespractice, tristan mckay, 10/29/21 2:37pm, version 0.0 
+
+# three common types of data
+
+lives_remaining = 5
+forty_time=4.34 # floating point, any +/- number with a decimal.
+death_cry = "when you cut one head off... another will appear" 
+# line 7 is a string -- collection of letters/number/other chars.
